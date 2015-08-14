64ac22ae2fddb26f7fdc0efe0ebacf466d67946b
MarkNOakden <mark.oakden@virtualclarity.com>
2015-08-14 16:12:44 +0100

    allowed_users check bugfix

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def home_page():
     user = users.get_current_user()
 
     if user:
-        if user in allowed_users:
+        if user.user_id in allowed_users:
             return "Hello " + user.nickname() + " bikeR is GO! (" + user.user_id() + ")"
         else:
             return redirect('/register')
